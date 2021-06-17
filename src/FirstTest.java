5a9e5c527ef01c512c02286d10d013ae7c2fa5e6
Artem Bels <belsartem@gmail.com>
2021-06-17 15:36:52 +0300

    Урок 2. Ex1: Конфигурация инструментов
    По шагам, указанным в видео уроках, создан проект и успешно запущен первый мобильный автотест.

---
+++ src/FirstTest.java
@@ -0,0 +1,40 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "androidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "D:/Downloads/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities); //так мы передаем все все capabilities нашему WebDriver'у
+    }
+
+    @After
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest() {
+
+        System.out.println("First Test Run");
+    }
+}
