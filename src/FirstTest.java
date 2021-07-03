460b5c639e9d98a3781825beb4778cf6e70949bc
Artem Bels <belsartem@gmail.com>
2021-07-03 14:37:32 +0300

    Основные настройки перенесены в CoreTestCase

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,49 +1,20 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URL;
 import java.util.List;
 
-public class FirstTest {
-
-    private AppiumDriver driver;
-
-    @Before
-    public void setUp() throws Exception {
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "androidTestDevice");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "D:/Downloads/JavaAppiumAutomation/apks/org.wikipedia.apk");
-        //capabilities.setCapability("appWaitActivity", "600");
-
-        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities); //так мы передаем все все capabilities нашему WebDriver'у
-    }
-
-    @After
-    public void tearDown() {
-
-        driver.quit();
-    }
+public class FirstTest extends CoreTestCase {
 
     @Test
-    public void firstTest() {
+    public void testSearch() {
 
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
@@ -174,7 +145,7 @@ public class FirstTest {
     }
 
     @Test
-    public void saveFirstArticleToMyList() {
+    public void testSaveFirstArticleToMyList() {
 
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
---
+++ src/lib/CoreTestCase.java
@@ -0,0 +1,39 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class CoreTestCase extends TestCase {
+
+    protected AppiumDriver driver;
+    private static String AppiumURL = "http://0.0.0.0:4723/wd/hub";
+
+    @Override
+    public void setUp() throws Exception {
+
+        super.setUp();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "androidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "D:/Downloads/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL(AppiumURL), capabilities); //так мы передаем все все capabilities нашему WebDriver'у
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+
+        driver.quit();
+
+        super.tearDown();
+    }
+}
