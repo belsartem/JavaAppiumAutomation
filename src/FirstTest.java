34474cda32a70ed30188712cc799248470e8d63c
Artem <belsartem@gmail.com>
2023-04-11 21:47:10 +0200

    В тест добавлены новые шаги: переходим в Википедию, кликаем на Search таб, далее в результатах в контейнере ищем результат по Object-oriented programming language
    Также добавлен отдельный метод с ожиданием и сообщением об ошибке. Метод override'инговый либо содержит дефолтное время до падения теста, либо настраивоемое под каждый отдельный тест

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -34,6 +38,32 @@ public class FirstTest {
 
     @Test
     public void firstTest() {
-        System.out.println("First test run");
+        WebElement elementToInitSearch = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+        elementToInitSearch.click();
+
+        WebElement elementToEnterSearchLine = waitForElementPresentByXpath(
+                "//*[contains(@text,'Search…')]",
+                "Cannot find search input"
+        );
+
+        elementToEnterSearchLine.sendKeys("Java");
+        waitForElementPresentByXpath(
+                "//*[@resource-id='org.wikipedia:id/search_container']//*[@text='Object-oriented programming language']",
+                "Cannot find 'Object-oriented programming language' topic searching by 'Java'",
+                15
+        );
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String errorMessage, long timeputInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeputInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String errorMessage) {
+        return waitForElementPresentByXpath(xpath, errorMessage, 5);
     }
 }
